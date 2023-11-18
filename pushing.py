cac4584930e81e74cf4e64b47a5cde76d7c67d5f
Rundll86 <2028673870@qq.com>
2023-11-18 16:57:18 +0800

    Update

--- pushing.py
+++ pushing.py
@@ -3,11 +3,12 @@ import os, sys
 if len(sys.argv) > 1:
     a = open("articleData/" + sys.argv[1] + ".content.html", "r", encoding="utf8")
     t = a.read()
-    t = t.replace("    ", "$blank4")
+    t = t.replace("    ", "$blank4;")
     a.close()
     a = open("articleData/" + sys.argv[1] + ".content.html", "w", encoding="utf8")
     a.write(t)
     a.close()
-os.system("git add .")
-os.system('git commit -m "Update"')
-os.system("git push -u origin main")
+else:
+    os.system("git add .")
+    os.system('git commit -m "Update"')
+    os.system("git push -u origin main")
