0af677f47d0b2ada40321e0b76d007940b6a8fcc
陨落基围虾 <3161880837@qq.com>
2026-06-06 19:34:30 +0800

    style(info, app): 统一代码缩进并更新项目列表与页面文案
    1. 将所有代码缩进从空格改为制表符统一格式
    2. 更新技能描述，补充游戏项目跳转链接
    3. 调整项目列表顺序并新增Patreon-Proxy工具项目

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,92 +1,93 @@
 <template>
-    <BackgroundImage :blur="bluring" />
-    <FullscreenSize class="container-app">
-        <div class="main" @mouseover="bluring = true" @mouseout="bluring = false">
-            <SelfInformation />
-            <DescriptionParagraph title="关于我">
-                你好！<br>
-                我是 <b>FallingShrimp</b>，来自SolariiX的 <b>全栈/游戏</b> 开发爱好者。<br>
-                有梦想，但仍在学习的路上，尚无法实现。
-            </DescriptionParagraph>
-            <DescriptionParagraph title="技能">
-                我讨厌 <b>vibe-coding</b>。<br>
-                我用 <b>Godot</b> 和 <b>Unity</b> 做过一些游戏，现不太精通。<br>
-                我热爱设计 <b>类型安全</b> / <b>高度IntelliSense</b> 的开发框架。<br>
-                <AlignBox>
-                    <span>
-                        语言
-                        <WhiteSpace :width="10" />
-                        <BlockLabel v-for="language in languages" :key="language">{{ language }}</BlockLabel>
-                    </span>
-                    <br>
-                    <span>
-                        技术栈
-                        <WhiteSpace :width="10" />
-                        <BlockLabel v-for="framework in frameworks" :key="framework">{{ framework }}</BlockLabel>
-                    </span>
-                    <br>
-                    <span>
-                        正在学习
-                        <WhiteSpace :width="10" />
-                        <BlockLabel v-for="learning in learnings" :key="learning">{{ learning }}</BlockLabel>
-                    </span>
-                </AlignBox>
-            </DescriptionParagraph>
-            <DescriptionParagraph title="XP">
-                只是一个变态兽人控罢了，bottom。<br>
-                <OutLink href="oc.jpg">
-                    <template #prompt>
-                        <img src="oc.jpg" class="oc-preview">
-                        <img src="avatars/shrimp-background.jpg" class="oc-preview">
-                    </template>
-                    自设/OC：希利普医生
-                </OutLink><br>
-                大尾巴白狼疑似命中注定。<br>
-                推荐观看：<br>
-                <FriendLink :people="specialFriends.lycaon" />
-                <FriendLink :people="specialFriends.death" />
-            </DescriptionParagraph>
-            <DescriptionParagraph title="联系方式">
-                邮箱：<OutLink href="mailto:3161880837@qq.com">3161880837@qq.com</OutLink><br>
-                QQ：<OutLink @click="copy('3161880837')">3161880837</OutLink><br>
-                <div class="links">
-                    <OutLink v-for="link in otherContactWay" :href="link[1]">{{ link[0] }}</OutLink>
-                </div>
-            </DescriptionParagraph>
-            <HorizontalLine />
-            <BigTitle>development</BigTitle><br>
-            <template v-for="project in projects" :key="project.title">
-                <ProjectCard :website="project.website" :image="project.image" :title="project.title"
-                    :tech-stack="project.techStack" :category="project.category">
-                    {{ project.description }}
-                </ProjectCard>
-            </template>
-            <HorizontalLine />
-            <BigTitle>友情链接</BigTitle><br>
-            <span>相关工作室</span>
-            <div class="friend-links">
-                <FriendLink v-for="people in studios" :people="people" :key="people.name" />
-            </div>
-            <span>高山流水，知音之交</span>
-            <div class="friend-links">
-                <FriendLink v-for="people in friends" :people="people" :key="people.name" />
-            </div>
-            <span>三人行，必有我师焉</span>
-            <div class="friend-links">
-                <FriendLink v-for="people in teachers" :people="people" :key="people.name" />
-            </div>
-            <span>蛋糕是个谎言</span>
-            <div class="friend-links">
-                <FriendLink v-for="people in cakeIsLie" :people="people" :key="people.name" />
-            </div>
-            <span>本网站的别名</span><br>
-            <AlignBox>
-                <template v-for="alias in aliases">
-                    <OutLink :href="`https://${alias}`">{{ alias }}</OutLink><br>
-                </template>
-            </AlignBox>
-        </div>
-    </FullscreenSize>
+	<BackgroundImage :blur="bluring" />
+	<FullscreenSize class="container-app">
+		<div class="main" @mouseover="bluring = true" @mouseout="bluring = false">
+			<SelfInformation />
+			<DescriptionParagraph title="关于我">
+				你好！<br>
+				我是 <b>FallingShrimp</b>，来自SolariiX的 <b>全栈/游戏</b> 开发爱好者。<br>
+				有梦想，但仍在学习的路上，尚无法实现。
+			</DescriptionParagraph>
+			<DescriptionParagraph title="技能">
+				我讨厌 <b>vibe-coding</b>。<br>
+				我用 <b>Godot</b> 和 <b>Unity</b> 做过一些游戏，尚未发布，详见
+				<OutLink href="https://github.com/Rundll86?tab=repositories">Github</OutLink>。<br>
+				我热爱设计 <b>类型安全</b> / <b>高度IntelliSense</b> 的开发框架。<br>
+				<AlignBox>
+					<span>
+						语言
+						<WhiteSpace :width="10" />
+						<BlockLabel v-for="language in languages" :key="language">{{ language }}</BlockLabel>
+					</span>
+					<br>
+					<span>
+						技术栈
+						<WhiteSpace :width="10" />
+						<BlockLabel v-for="framework in frameworks" :key="framework">{{ framework }}</BlockLabel>
+					</span>
+					<br>
+					<span>
+						正在学习
+						<WhiteSpace :width="10" />
+						<BlockLabel v-for="learning in learnings" :key="learning">{{ learning }}</BlockLabel>
+					</span>
+				</AlignBox>
+			</DescriptionParagraph>
+			<DescriptionParagraph title="XP">
+				只是一个变态兽人控罢了，bottom。<br>
+				<OutLink href="oc.jpg">
+					<template #prompt>
+						<img src="oc.jpg" class="oc-preview">
+						<img src="avatars/shrimp-background.jpg" class="oc-preview">
+					</template>
+					自设/OC：希利普医生
+				</OutLink><br>
+				大尾巴白狼疑似命中注定。<br>
+				推荐观看：<br>
+				<FriendLink :people="specialFriends.lycaon" />
+				<FriendLink :people="specialFriends.death" />
+			</DescriptionParagraph>
+			<DescriptionParagraph title="联系方式">
+				邮箱：<OutLink href="mailto:3161880837@qq.com">3161880837@qq.com</OutLink><br>
+				QQ：<OutLink @click="copy('3161880837')">3161880837</OutLink><br>
+				<div class="links">
+					<OutLink v-for="link in otherContactWay" :href="link[1]">{{ link[0] }}</OutLink>
+				</div>
+			</DescriptionParagraph>
+			<HorizontalLine />
+			<BigTitle>development</BigTitle><br>
+			<template v-for="project in projects" :key="project.title">
+				<ProjectCard :website="project.website" :image="project.image" :title="project.title"
+					:tech-stack="project.techStack" :category="project.category">
+					{{ project.description }}
+				</ProjectCard>
+			</template>
+			<HorizontalLine />
+			<BigTitle>友情链接</BigTitle><br>
+			<span>相关工作室</span>
+			<div class="friend-links">
+				<FriendLink v-for="people in studios" :people="people" :key="people.name" />
+			</div>
+			<span>高山流水，知音之交</span>
+			<div class="friend-links">
+				<FriendLink v-for="people in friends" :people="people" :key="people.name" />
+			</div>
+			<span>三人行，必有我师焉</span>
+			<div class="friend-links">
+				<FriendLink v-for="people in teachers" :people="people" :key="people.name" />
+			</div>
+			<span>蛋糕是个谎言</span>
+			<div class="friend-links">
+				<FriendLink v-for="people in cakeIsLie" :people="people" :key="people.name" />
+			</div>
+			<span>本网站的别名</span><br>
+			<AlignBox>
+				<template v-for="alias in aliases">
+					<OutLink :href="`https://${alias}`">{{ alias }}</OutLink><br>
+				</template>
+			</AlignBox>
+		</div>
+	</FullscreenSize>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
@@ -109,31 +110,31 @@ const bluring = ref(false);
 </script>
 <style scoped>
 .container-app {
-    text-align: center;
-    width: 100%;
-    height: auto;
-    position: absolute;
+	text-align: center;
+	width: 100%;
+	height: auto;
+	position: absolute;
 }
 
 .main {
-    padding: 50px 0;
-    display: inline-block;
+	padding: 50px 0;
+	display: inline-block;
 }
 
 .links {
-    display: inline-flex;
-    gap: 5px;
+	display: inline-flex;
+	gap: 5px;
 }
 
 .oc-preview {
-    width: 200px;
-    display: block;
-    margin: 5px;
+	width: 200px;
+	display: block;
+	margin: 5px;
 }
 
 .text-left {
-    text-align: left;
-    width: fit-content;
-    display: inline-block;
+	text-align: left;
+	width: fit-content;
+	display: inline-block;
 }
 </style>
--- src/info.ts
+++ src/info.ts
@@ -1,139 +1,139 @@
 import { PeopleDescriptor, ProjectData } from "./structs";
 
 export const labels: string[] = [
-    "完美主义",
-    "Web 全栈 / 游戏开发",
-    "变态兽人控 / Gay(Bottom)",
-    "SolariiX",
-    "Rogue / 类银 / 类魂 重度爱好者"
+	"完美主义",
+	"Web 全栈 / 游戏开发",
+	"变态兽人控 / Gay(Bottom)",
+	"SolariiX",
+	"Rogue / 类银 / 类魂 重度爱好者"
 ];
 export const email = "3161880837@qq.com";
 export const qq = 3161880837;
 export const otherContactWay: [string, string][] = [
-    ["哔哩哔哩", "https://space.bilibili.com/649063815"],
-    ["Github", "https://github.com/Rundll86"],
-    ["X/Twitter", "https://x.com/fallingshrimp"]
+	["哔哩哔哩", "https://space.bilibili.com/649063815"],
+	["Github", "https://github.com/Rundll86"],
+	["X/Twitter", "https://x.com/fallingshrimp"]
 ];
 
 export const languages: string[] = [
-    "TypeScript / JavaScript",
-    "Rust / C#",
-    "GDScript / Python"
+	"TypeScript / JavaScript",
+	"Rust / C#",
+	"GDScript / Python"
 ];
 export const frameworks: string[] = [
-    "Vue / Nine9 (Webpack/tsup)",
-    "Hono.js / Flask",
-    "Tauri",
-    "Unity / Godot.NET",
-    "TModLoader",
+	"Vue / Nine9 (Webpack/tsup)",
+	"Hono.js / Flask",
+	"Tauri",
+	"Unity / Godot.NET",
+	"TModLoader",
 ];
 export const learnings: string[] = [
-    "片段着色器",
-    "2D骨骼动画",
-    "FSM Animator",
-    "Tauri"
+	"片段着色器",
+	"2D骨骼动画",
+	"FSM Animator",
+	"Tauri"
 ];
 export const studios: PeopleDescriptor[] = [
-    {
-        name: "SolariiX",
-        website: "https://solariix.com/",
-        description: "一家成立于2022年的独立游戏工作室。"
-    },
+	{
+		name: "SolariiX",
+		website: "https://solariix.com/",
+		description: "一家成立于2022年的独立游戏工作室。"
+	},
 ];
 export const teachers: PeopleDescriptor[] = [
-    {
-        name: "熊谷 凌",
-        website: "https://github.com/FurryR/",
-        description: "什么都写的全栈大佬，正在编写自己的博客，不谈对象。",
-        avatar: "FurryR"
-    },
+	{
+		name: "熊谷 凌",
+		website: "https://github.com/FurryR/",
+		description: "什么都写的全栈大佬，正在编写自己的博客，不谈对象。",
+		avatar: "FurryR"
+	},
 ];
 export const cakeIsLie: PeopleDescriptor[] = [
-    {
-        name: "Cyberexplorer",
-        website: "https://lanwywritexu.github.io/",
-        description: "不要看着我以为我很傻，其实我一点也不聪明。"
-    },
-    {
-        name: "子昭的技术博客",
-        website: "https://tech.zeechow.eu.cc",
-        description: "",
-        avatar: "1"
-    }
+	{
+		name: "Cyberexplorer",
+		website: "https://lanwywritexu.github.io/",
+		description: "不要看着我以为我很傻，其实我一点也不聪明。"
+	},
+	{
+		name: "子昭的技术博客",
+		website: "https://tech.zeechow.eu.cc",
+		description: "",
+		avatar: "1"
+	}
 ];
 export const specialFriends: Record<"lycaon" | "death", PeopleDescriptor> = {
-    lycaon: {
-        name: "冯·莱卡恩",
-        website: "https://e621.net/posts?tags=von_lycaon",
-        description: "哎呀主播你怎么这么帅🥰看得我春水都要泛滥了🥰",
-        avatar: "lycaon",
-        dangerous: true
-    },
-    death: {
-        name: "Death",
-        website: "https://e621.net/posts?tags=death_(puss_in_boots)",
-        description: "哎呀主播你怎么这么帅🥰看得我春水都要泛滥了🥰",
-        avatar: "death",
-        dangerous: true
-    }
+	lycaon: {
+		name: "冯·莱卡恩",
+		website: "https://e621.net/posts?tags=von_lycaon",
+		description: "哎呀主播你怎么这么帅🥰看得我春水都要泛滥了🥰",
+		avatar: "lycaon",
+		dangerous: true
+	},
+	death: {
+		name: "Death",
+		website: "https://e621.net/posts?tags=death_(puss_in_boots)",
+		description: "哎呀主播你怎么这么帅🥰看得我春水都要泛滥了🥰",
+		avatar: "death",
+		dangerous: true
+	}
 };
 export const friends: PeopleDescriptor[] = [
-    {
-        name: "阳毅",
-        website: "https://yangyiit.top/",
-        description: "保持热爱, 奔赴山海。"
-    },
-    {
-        name: "TangDo158",
-        website: "https://github.com/TangDo158",
-        description: "也许是情绪不够稳定（存疑）。"
-    },
-    {
-        name: "翟十光的博客",
-        website: "https://zhaishis.com",
-        description: "春江花朝秋月夜，往往取酒还独倾",
-        avatar: "ssg"
-    }
+	{
+		name: "阳毅",
+		website: "https://yangyiit.top/",
+		description: "保持热爱, 奔赴山海。"
+	},
+	{
+		name: "TangDo158",
+		website: "https://github.com/TangDo158",
+		description: "也许是情绪不够稳定（存疑）。"
+	},
+	{
+		name: "翟十光的博客",
+		website: "https://zhaishis.com",
+		description: "春江花朝秋月夜，往往取酒还独倾",
+		avatar: "ssg"
+	}
 ];
 export const projects: ProjectData[] = [
-    {
-        website: "https://github.com/Rundll86/script-editor-2",
-        image: "se2.jpg",
-        title: "ScriptEditor",
-        techStack: "Vue",
-        category: "工具",
-        description: "基于GUI的游戏剧本设计器，让不会写代码的文案师也能组织游戏剧本。"
-    },
-    {
-        website: "/fs-context",
-        image: "fsc.jpg",
-        title: "FS-Context",
-        techStack: "TypeScript",
-        category: "框架",
-        description: "类型安全且灵活易用的框架模板，用于开发某软件的通用拓展程序。"
-    },
-    {
-        website: "https://github.com/Rundll86/NDDrone-SDK",
-        image: "nds.jpg",
-        title: "NDDrone-SDK",
-        techStack: "Python",
-        category: "软件",
-        description: "基于SSVEP BCI接口技术实现的 脑机接口-无人机飞控 桥接技术。"
-    },
-    {
-        website: "https://github.com/Rundll86/nine",
-        image: "nine.jpg",
-        title: "Nine-9",
-        techStack: "Vanilla",
-        category: "框架",
-        description: "一个轻量、超高性能、类型安全的 Vanilla DOM 响应式 UI 框架。"
-    }
+	{
+		website: "https://github.com/Rundll86/NDDrone-SDK",
+		image: "nds.jpg",
+		title: "NDDrone-SDK",
+		techStack: "Python",
+		category: "软件",
+		description: "基于SSVEP BCI接口技术实现的 脑机接口-无人机飞控 桥接技术。"
+	},
+	{
+		website: "https://github.com/Rundll86/nine",
+		image: "nine.jpg",
+		title: "Nine-9",
+		techStack: "Vanilla",
+		category: "框架",
+		description: "一个轻量、超高性能、类型安全的 Vanilla DOM 响应式 UI 框架。"
+	},
+	{
+		website: "https://github.com/Rundll86/BeastBeat-proxy",
+		image: "fsc.jpg",
+		title: "Patreon-Proxy",
+		techStack: "Python",
+		category: "工具",
+		description: "将Patreon OAuth2流量代理，实现可绕过部分游戏的Patreon登录验证。"
+	},
+	{
+		website: "https://github.com/Rundll86/script-editor-2",
+		image: "se2.jpg",
+		title: "ScriptEditor",
+		techStack: "Vue",
+		category: "工具",
+		description: "基于GUI的游戏剧本设计器，让不会写代码的文案师也能组织游戏剧本。"
+	},
 ];
 
 export const aliases = [
-    "fshrimp.top",
-    "von-lycaon.我爱你",
-    "1145141919810.website",
-    "91vip.website",
-    "917891789178.xyz"
+	"fshrimp.top",
+	"von-lycaon.我爱你",
+	"1145141919810.website",
+	"91vip.website",
+	"917891789178.xyz"
 ];
